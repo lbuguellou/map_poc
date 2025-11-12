eeb9392e193a3184e87d27cc3ad3324462c3b3d1
Laetitia Buguellou <laetitiabuguellou@mac.home>
2025-11-12 14:15:25 +0100

    add infos on address

--- map-streamlit/map.py
+++ map-streamlit/map.py
@@ -23,9 +23,11 @@ def map_search():
     location = geocode.json() 
 
     if location :
-        st.session_state["address"] = city
+        st.session_state["locality"] = location["locality"]
+        st.session_state["address"] = location["address"]
         st.session_state["latitude"] = location["latitude"]
         st.session_state["longitude"] = location["longitude"]
+        st.session_state["infos"] = location["infos"]
         st.session_state["radius"] = radius
         st.session_state["nb_results"] = nb_results
 
@@ -47,6 +49,7 @@ button = st.sidebar.button("Rechercher", on_click=map_search)
 
 if "address" in st.session_state :
     #Display infos for location
+    st.sidebar.text(f"Ville : {st.session_state['locality']}")
     st.sidebar.text(f"Infos : {st.session_state['address']}")
     st.sidebar.text(f"Latitude : {st.session_state['latitude']}")
     st.sidebar.text(f"Longitude : {st.session_state['longitude']}")
@@ -56,17 +59,17 @@ if "address" in st.session_state :
 
     folium.Marker(
         location=[st.session_state["latitude"], st.session_state["longitude"]],
-        popup=st.session_state['address'],
+        popup=st.session_state["address"],
         icon=folium.Icon(color="blue")
     ).add_to(map)
 
 
     if "nearby_places" in st.session_state :
-        for city_item in st.session_state['nearby_places']:
-            if st.session_state["address"] != city_item['address']:
+        for city_item in st.session_state["nearby_places"]:
+            if st.session_state["locality"] != city_item["locality"]:
                 folium.Marker(
-                    location=[city_item['latitude'], city_item['longitude']],
-                    popup=city_item['address'],
+                    location=[city_item["latitude"], city_item["longitude"]],
+                    popup=city_item["address"],
                     icon=folium.Icon(color="green")
                 ).add_to(map)
 
@@ -78,11 +81,11 @@ st.subheader(f"Liste", divider="gray")
 
 if "nearby_places" in st.session_state :
     config = {
-        'address' : st.column_config.TextColumn('Nom', width='large'),
-        'latitude' : st.column_config.NumberColumn('Latitude'),
-        'longitude' : st.column_config.NumberColumn('Longitude')
+        "locality" : st.column_config.TextColumn("Nom", width="large"),
+        "latitude" : st.column_config.NumberColumn("Latitude"),
+        "longitude" : st.column_config.NumberColumn("Longitude")
     }
-    df = pd.DataFrame(data=st.session_state["nearby_places"], columns=['address', 'latitude', 'longitude'])
-    st.dataframe(df, width='stretch', hide_index=True, column_config=config)
+    df = pd.DataFrame(data=st.session_state["nearby_places"], columns=["locality", "latitude", "longitude"])
+    st.dataframe(df, width="stretch", hide_index=True, column_config=config)
 
 
